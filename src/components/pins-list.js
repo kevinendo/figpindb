abdf6dad1394bea080a1e148fd2f995d0c650d34
Kevin Endo <kevinendo@gmail.com>
2022-03-23 10:32:09 -0700

    mha release

--- src/components/pins-list.js
+++ src/components/pins-list.js
@@ -21,9 +21,9 @@ let prevPage = "";
 let nextPage = "";
 
 if (parseInt(page) === 0)  {
-  prevPage = "/pins?page=19";
+  prevPage = "/pins?page=20";
   nextPage = "/pins?page=" + (parseInt(page)+1);
-} else if (parseInt(page) === 19) {
+} else if (parseInt(page) === 20) {
   prevPage = "/pins?page=" + (parseInt(page)-1);
   nextPage = "/pins?page=0";
 } else {
@@ -121,6 +121,7 @@ useEffect(() => {
         { (parseInt(page) === 17) ? (<a className="active" href="/pins?page=17">17</a>) : ( <a href="/pins?page=17">17</a>) }
         { (parseInt(page) === 18) ? (<a className="active" href="/pins?page=18">18</a>) : ( <a href="/pins?page=18">18</a>) }
         { (parseInt(page) === 19) ? (<a className="active" href="/pins?page=19">19</a>) : ( <a href="/pins?page=19">19</a>) }
+        { (parseInt(page) === 20) ? (<a className="active" href="/pins?page=20">20</a>) : ( <a href="/pins?page=20">20</a>) }
         <a href={nextPage}>&gt;</a>
         </div>
         </div>
